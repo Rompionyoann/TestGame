f0c3858eff0ae50a48cc02435e3828ba957c0b98
Rompionyoann <45792453+Rompionyoann@users.noreply.github.com>
2024-04-26 10:39:40 +0200

    first commit

---
+++ src/client/Controllers/TemplateController.lua
@@ -0,0 +1,26 @@
+-- Services
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+-- Knit packages
+local Packages = ReplicatedStorage.Packages
+local Knit = require(Packages.Knit)
+
+-- Player
+local player = Players.LocalPlayer
+
+-- TemplateController
+local TemplateController = Knit.CreateController({
+	Name = "TemplateController",
+
+})
+
+--|| Local Functions ||--
+
+--|| Functions ||--
+
+function TemplateController:KnitStart()
+	print("bonjour ultim")
+end
+
+return TemplateController
